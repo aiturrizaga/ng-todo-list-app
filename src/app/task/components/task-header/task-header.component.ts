a7926665b96d24addcad5e5def232e80e199f188
Alvaro Iturrizaga <aiturrizaga@vallegrande.edu.pe>
2023-09-01 01:02:15 -0500

    Multiple components ready

--- src/app/task/components/task-header/task-header.component.ts
+++ src/app/task/components/task-header/task-header.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-task-header',
   templateUrl: './task-header.component.html',
-  styleUrls: ['./task-header.component.css']
+  styleUrls: ['./task-header.component.css'],
 })
 export class TaskHeaderComponent {
+  @Input({required: true})
+  public title: string = '';
 
+  @Input()
+  public date: string = '';
+
+  @Input()
+  public course: string = '';
 }
--- src/app/task/components/task-input/task-input.component.ts
+++ src/app/task/components/task-input/task-input.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Task } from '../../interfaces/task.interface';
 
 @Component({
   selector: 'app-task-input',
   templateUrl: './task-input.component.html',
-  styleUrls: ['./task-input.component.css']
+  styleUrls: ['./task-input.component.css'],
 })
 export class TaskInputComponent {
+  @Output() createTask: EventEmitter<any> = new EventEmitter<any>();
+  public newTask: string = '';
 
+  addTask() {
+    const task: Task = {
+      title: this.newTask,
+      completed: false,
+    };
+    this.createTask.emit(task);
+  }
 }
